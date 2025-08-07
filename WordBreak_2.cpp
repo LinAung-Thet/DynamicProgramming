f3ae7cc31cf8e3c8fc7e5a039d630726a4768788
LinAung-Thet <laungthet@gmail.com>
2025-08-07 23:47:25 +1200

    Created WordBreak_2.cpp
    - Added a program to compute all possible sentences that can be formed from a given string using a provided word dictionary.

---
+++ WordBreak_2.cpp
@@ -0,0 +1,82 @@
+// https://leetcode.com/problems/word-break-ii/description/?envType=problem-list-v2&envId=dynamic-programming
+
+#include<vector>
+#include<iostream>
+#include<string>
+#include<unordered_set>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        unordered_map<string, vector<string>> memo;
+        return dfs(s, dict, memo);
+    }
+
+private:
+    vector<string> dfs(const string& s, unordered_set<string>& dict,
+                       unordered_map<string, vector<string>>& memo) {
+        if (memo.count(s)) return memo[s];
+        if (s.empty()) return {""};
+
+        vector<string> result;
+        for (int i = 1; i <= s.size(); ++i) {
+            string word = s.substr(0, i);
+            if (dict.count(word)) {
+                vector<string> suffixes = dfs(s.substr(i), dict, memo);
+                for (const string& suffix : suffixes) {
+                    string sentence = word + (suffix.empty() ? "" : " " + suffix);
+                    result.push_back(sentence);
+                }
+            }
+        }
+
+        memo[s] = result;
+        return result;
+    }
+};
+
+//Test cases
+int main() {
+    Solution solution;
+    string s;
+    vector<string> result;
+    vector<string> wordDict;
+
+    cout << "Test case 1" << endl;
+    s = "catsanddog";
+    wordDict = {"cat","cats","and","sand","dog"};
+    result = solution.wordBreak(s, wordDict);
+    cout << "Expected : [cats and dog, cat sand dog]" << endl;
+    cout << "Result   : [";
+    for (auto sentence : result) {
+        cout << sentence << ", ";
+    }
+    cout << "]" << endl << endl;
+
+    cout << "Test case 2" << endl;
+    s = "pineapplepenapple";
+    wordDict = {"apple","pen","applepen","pine","pineapple"};
+    result = solution.wordBreak(s, wordDict);
+    cout << "Expected : [pine apple pen apple, pineapple pen apple, pine applepen apple]" << endl;
+    cout << "Result   : [";
+    for (auto sentence : result) {
+        cout << sentence << ", ";
+    }
+    cout << "]" << endl << endl;
+
+    cout << "Test case 3" << endl;
+    s = "catsandog";
+    wordDict = {"cats","dog","sand","and","cat"};
+    result = solution.wordBreak(s, wordDict);
+    cout << "Expected : []" << endl;
+    cout << "Result   : [";
+    for (auto sentence : result) {
+        cout << sentence << ", ";
+    }
+    cout << "]" << endl << endl;
+
+    return 0;
+}
